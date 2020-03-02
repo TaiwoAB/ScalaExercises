0c5d80cef04ad984280655db116238177656016b
TaiwoAB <tolaainabadejo@gmail.com>
2020-03-02 17:40:13 +0000

    Question1

---
+++ src/main/scala/Day1/Assignment.scala
@@ -0,0 +1,12 @@
+package Day1
+
+object Assignment extends App{
+ var message ="Hello world"
+ println(message)
+ val a =1
+ val b=10
+ val c=7
+  var listed= List(a,b,c).sorted
+
+  println(listed(0)*listed(1))
+}
---
+++ src/main/scala/Day1/Question1.scala
@@ -0,0 +1,12 @@
+package Day1
+
+object Question1 extends App{
+  def result(a: Int, b: Int, c: Int): Int = {
+    var numList = List(a, b, c).sorted
+
+    (numList(0) + numList(1))* numList (2)
+  }
+  println(result(3,9,3))
+
+}
+
