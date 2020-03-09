23026ccf7e6a4aa2e43bbe81cddf4142c47ac95b
TaiwoAB <tolaainabadejo@gmail.com>
2020-03-09 10:06:15 +0000

    Rock paper scissors

--- src/main/scala/Day2/Strings.scala
+++ src/main/scala/Day2/Strings.scala
@@ -1,12 +1,13 @@
 package Day2
 
 object Strings extends App {
- def results(name:String, num:Int):StringBuilder={
+ def results(name:String, num:Int):String={
    val res= new StringBuilder("")
    for (a<-name.length-num to name.length-1){
      res.append(name.charAt(a))
    }
-   res
+
+   res.toString()
  }
   println(results("Hello", 3))
 }
